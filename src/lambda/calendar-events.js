f2c7015f774591b9c05ac9b30b1d508557f30a16
Paul Hosler <pahosler@gmail.com>
2019-04-19 16:54:40 -0500

    bug fixes and env vars!

--- src/lambda/calendar-events.js
+++ src/lambda/calendar-events.js
@@ -1,38 +1,22 @@
+require('dotenv').config()
 const { google } = require('googleapis')
 
-// const TOKEN = process.env.TOKEN
-// const CREDENTIALS = process.env.CREDENTIALS
-// const CALENDAR_ID = process.env.CALENDAR_ID
-
-const TOKEN = '{"access_token":"ya29.GlvrBnCImILDrwAjzMq2rkvxE6Ex8StL9SnTCSgskSAdQnfTXrtybNeEYU574gsHayAevmLXgfV4QduUHdRh94MIGcJsp7boT_XrkxcdcK_DgRZAIA7TtBPZxopl","refresh_token":"1/tJOLunTLM7MlvtCQlDSx5ZixMkSleVU3Qcra8GbC_J4","scope":"https://www.googleapis.com/auth/calendar.readonly","token_type":"Bearer","expiry_date":1555207789172}'
-const CREDENTIALS = '{"installed": {"client_id": "701841266465-tofh1lmhavhkcnkej26akgn4vtm9203l.apps.googleusercontent.com","project_id": "quickstart-1554758372424","auth_uri": "https://accounts.google.com/o/oauth2/auth","token_uri": "https://oauth2.googleapis.com/token","auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs","client_secret": "ZJzqTwMqcp3ZJvMljfURbhlb","redirect_uris": ["urn:ietf:wg:oauth:2.0:oob", "http://localhost"]}}'
-const CALENDAR_ID = 'k00j76b2k0p976p3uncsfqduo8@group.calendar.google.com'
-
-
-const getCredentials = () => {
-  if (CREDENTIALS) {
-    return JSON.parse(CREDENTIALS)
-  }
-  throw new Error('Unable to load credentials')
-}
-
-const getToken = () => {
-  if (TOKEN) {
-    return JSON.parse(TOKEN)
-  }
-  throw new Error('Unable to load token')
-}
+const MAX_RESULTS = process.env.MAX_RESULTS
+const CALENDAR_ID = process.env.CALENDAR_ID
 
 const getEvents = async () => {
-  let credentials = getCredentials()
-  const {
-    client_secret,
-    client_id,
-    redirect_uris
-  } = credentials.installed
+  let token = {
+    access_token: process.env.ACCESS_TOKEN,
+    refresh_token: process.env.REFRESH_TOKEN,
+    scope: "https://www.googleapis.com/auth/calendar.readonly",
+    token_type: "Bearer",
+    expiry_date: process.env.EXPIRY_DATE
+  }
+  let client_id = process.env.CLIENT_ID
+  let client_secret = process.env.CLIENT_SECRET
+  let redirect_uris = process.env.REDIRECT_URI
 
-  const token = getToken()
-  const oAuth2Client = await new google.auth.OAuth2(client_id, client_secret, redirect_uris[0])
+  const oAuth2Client = await new google.auth.OAuth2(client_id, client_secret, redirect_uris)
   oAuth2Client.setCredentials(token)
 
   const calendar = google.calendar({
@@ -43,12 +27,11 @@ const getEvents = async () => {
   return await calendar.events.list({
     calendarId: CALENDAR_ID,
     timeMin: (new Date()).toISOString(),
-    maxResults: 10,
+    maxResults: MAX_RESULTS || 3,
     singleEvents: true,
     orderBy: 'startTime'
   })
 }
-
 
 exports.handler = (event, context, callback) => {
   getEvents().then(res => {
@@ -73,7 +56,8 @@ exports.handler = (event, context, callback) => {
         statusCode: 200,
         headers: {
           "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Headers": "Content-Type"
+          "Access-Control-Allow-Headers": "Content-Type",
+          'Access-Control-Allow-Headers': 'application/json',
         },
         body: JSON.stringify(res)
       })
@@ -81,5 +65,4 @@ exports.handler = (event, context, callback) => {
     .catch(e => {
       callback(e)
     })
-
 }
